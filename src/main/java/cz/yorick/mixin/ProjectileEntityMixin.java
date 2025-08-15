d728dc8a0689b3b533d6bc4b2d4397cde4539fd8
Yorick_06 <unconfigured@null.spigotmc.org>
2025-08-15 19:59:26 +0200

    shadow projectiles now pass through entities they cannot hurt

--- src/main/java/cz/yorick/mixin/ProjectileEntityMixin.java
+++ src/main/java/cz/yorick/mixin/ProjectileEntityMixin.java
@@ -1,5 +1,7 @@
 package cz.yorick.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import cz.yorick.data.NecromancyAttachments;
 import cz.yorick.util.Util;
 import net.minecraft.entity.Entity;
@@ -26,5 +28,11 @@ public abstract class ProjectileEntityMixin extends Entity {
     private void necromancers_shadow$setOwner(@Nullable Entity owner, CallbackInfo info) {
         //when changing owner, check if the projectile should be a shadow
         NecromancyAttachments.markAsShadow(this, Util.isShadow(this.owner));
+    }
+
+    //shadow projectile cannot hit a friendly shadow/owner
+    @WrapMethod(method = "canHit")
+    protected boolean necromancers_shadow$canHit(Entity entity, Operation<Boolean> original) {
+        return original.call(entity) && Util.canHurt(this, entity);
     }
 }
